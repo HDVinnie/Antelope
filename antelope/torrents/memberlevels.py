94e114f001e9dcd7915e96898e17330c7e89e23f
= <wintermarkedward@gmail.com>
2015-12-31 22:15:54 +0000

    Remove now unused files

--- antelope/torrents/memberlevels.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class MemberLevels(Enum):
-    Member = 0
-    PowerUser = 50
-    SysOp = 100
--- antelope/torrents/paranoialevels.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class ParanoiaLevels(Enum):
-    Off = 0
-    High = 100
